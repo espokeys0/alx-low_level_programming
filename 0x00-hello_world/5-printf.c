69bb310a41075a3a015178dac929c992924afa02
Espokeys_king <espoirclavier0@gmail.com>
2022-09-10 01:53:55 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
 *main - prints a text
 *
 *Return: Always 0 (Success)
 */
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
 
