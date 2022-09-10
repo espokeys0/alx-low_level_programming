885f76d5fb2ea07d724b71e619b56e9cb95a88c7
Espokeys_king <espoirclavier0@gmail.com>
2022-09-10 00:06:59 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
-#!/bin/bash
-#include <stdio.h>
-/**
-* main - entry point
-*
-* return: 0 after printing the function
-*/
-int main(void)
-{puts("\"programing is like building a multilingual puzzle");
-     return(0);
+#include <stdio.h>                                                                                           
+
+/**                                                                                                          
+
+* main - entry point                                                                                         
+
+*                                                                                                            
+
+* return: 0 after printing the function                                                                      
+
+*/                                                                                                           
+
+int main(void)                                                                                               
+
+{puts("\"programing is like building a multilingual puzzle");                                                
+
+     return(0);                                                                                              
+
 }
