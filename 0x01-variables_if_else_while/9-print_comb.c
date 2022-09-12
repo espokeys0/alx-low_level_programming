c6fa4e95cfffd3e53f68f0ed10412b98913fd994
Espokeys_king <espoirclavier0@gmail.com>
2022-09-12 08:36:24 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - print if the number is positive, zero, or negative
+*
+*Description: using the main function
+*this program prints "programming is positive, zero, or negative
+*Return: 0
+*/
+int main(void)
+{int c;
+for (c = '0'; c <= '9'; c++)
+{putchar(c);
+ if(c != '9')
+ {putchar(',');
+  putchar(' '); } }
+putchar('\n');
+return (0); }
