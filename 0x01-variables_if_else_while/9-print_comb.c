d22572c1aebf2092160e90f9e02719ae698fef1f
Espokeys_king <espoirclavier0@gmail.com>
2022-09-12 08:44:38 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,8 @@ int main(void)
 {int c;
 for (c = '0'; c <= '9'; c++)
 {putchar(c);
- if(c != '9')
- {putchar(',');
-  putchar(' '); } }
+if (c != '9')
+{putchar(',');
+putchar(' '); } }
 putchar('\n');
 return (0); }
