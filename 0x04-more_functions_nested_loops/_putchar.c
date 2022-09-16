369b9269757f983ee02d57daecaaac70bb4f6aee
Espokeys_king <espoirclavier0@gmail.com>
2022-09-16 05:21:28 -0700

    Task 0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include "main.h"
+
 /**
  *
  * * _putchar - writes the character c to stdout
@@ -13,6 +14,7 @@
  * * On error, -1 is returned, and errno is set appropriately.
  *
  * */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
