a124e575e930007b24e824fc17dd736ab14cbe48
Espokeys <espoirclavier0@gmail.com>
2022-09-19 13:32:12 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
 *_puts - prints a string
 *@str: String to print
@@ -17,4 +16,5 @@ void _puts(char *str)
 		putchar(*(str + i));
 		i++;
        	}
-	putchar(10); }
+	putchar(10);
+}
