32a6e7733a2c21de6421a8534dbe798ab2904bdb
Espokeys <espoirclavier0@gmail.com>
2022-09-19 15:03:42 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*puts2 - print every other character
+*@str: String
+Ã*Return: nothing
+*/
+
+void puts2(char *str);
+{
+int i = 0;
+
+while (*(str + i) != '\	0')
+{
+if (i % 2 == 0)
+}
+putchar(*(str + i));
+putcher(10);
+}
