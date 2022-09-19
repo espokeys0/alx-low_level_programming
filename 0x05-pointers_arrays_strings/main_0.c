06ba908f0c87eec845d1d26a02c1b04e1e3f77fb
Espokeys <espoirclavier0@gmail.com>
2022-09-19 13:26:52 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void _puts(char *);
+
+/**
+
+ * main - vérifiez le code pour les élèves de Holberton School.
+
+ *
+
+ * Retour : toujours 0.
+
+ */
+
+int principal (vide)
+
+{
+
+	char *str;
+
+	str = "Holberton !" ;
+
+	_puts(str);
+
+	retour (0);
+
+}
