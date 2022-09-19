e47215294831737bef09b7d88416e8d48cdf9853
Espokeys <espoirclavier0@gmail.com>
2022-09-19 11:26:08 -0700

    Task 0

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,10 +4,10 @@
 *swap_int - swaps two integer value
 *@a: first integer
 *@b: second integer
-*Return: nothing
+*Return: return nothing
 */
 
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
 	int tmp = *a;
 	*a = *b;
