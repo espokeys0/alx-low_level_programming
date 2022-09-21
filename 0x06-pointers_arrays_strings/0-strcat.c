114cb46c2f1abaedd5671a1ac229f32bdb55ec69
Espokeys <espoirclavier0@gmail.com>
2022-09-21 22:54:23 +0200

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_strcat - concatenates two strings
+*@dest: String
+*@src: String
+*Return: nothing
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
+
+	
+
