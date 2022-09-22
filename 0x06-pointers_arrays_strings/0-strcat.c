15c5d8991f694216ae93eb3bbce80dad29c6478a
Espokeys <espoirclavier0@gmail.com>
2022-09-23 01:14:45 +0200

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,7 @@
 char *_strcat(char *dest, char *src)
 {
 int index = 0;
-int dest_len = 0; 
-
-while (dest[index++])
+int dest_len = 0; while (dest[index++])
 {
 dest_len++;
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
-void _putchar(void);
+ 
+/**
+*void prototypes()
+*int prototypes()
+*char prototypes()
+*/
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
