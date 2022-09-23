6252973cce71e0754b00562abf529bd39cf5c425
Espokeys <espoirclavier0@gmail.com>
2022-09-23 18:50:11 +0200

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,31 +3,19 @@
 
 
 /**
+ *print_numbers - checks for a digit (0 through 9).
  *
- * * print_numbers - checks for a digit (0 through 9).
- *
- * *
- *
- * * Return: Always 0.
- *
+  *Return: Always 0.
  * */
 
 void print_numbers(void)
-
 {
-
 	int c;
 
-
-
 	for (c = 48; c < 58; c++)
-
 	{
-
 		_putchar(c);
-
 	}
 
 	_putchar('\n');
-
 }
