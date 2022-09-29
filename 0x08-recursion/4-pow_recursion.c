181246bab87604b9d9c0f4ced30daaaf38dec57a
Espokeys <espoirclavier0@gmail.com>
2022-09-29 17:56:58 +0200

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,20 +2,22 @@
 
 /**
  *_pow_recursion - Return the value of x raised to the power of y
- *@x: the number to be raised
- *@y: the power
+ *@x: integer
+ *@y: integer
  *
- *Return: the value of x raised to be power of y
+ *Return: power recursion
  */
 
 int _pow_recursion(int x, int y)
 {
-int result = x;
 
 if (y < 0)
-
+{
 return (-1);
-else if (y == 0)
+}
+else if (y != 0)
+return (x * _pow_recursion(x, y - 1));
 
+else
 return (1);
 }
