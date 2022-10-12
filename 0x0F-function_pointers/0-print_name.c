c0d105b2a89684d142e6bbbbbe0c5350f3dccea6
Espokeys <espoirclavier0@gmail.com>
2022-10-13 01:05:52 +0200

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,9 +7,10 @@
  *
  *Return: void
  */
- void print-name(char *name, void (*f)(char *))
+
+void print_name(char *name, void (*f)(char *))
  {
 
- if (name && f)
- f(name)
+ if (name != NULL && f != NULL)
+ f(name);
  }
