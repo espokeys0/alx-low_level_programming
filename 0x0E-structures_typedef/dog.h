88ff2e3e8b3ab9ab1d54f1762b2defcc8b2117c4
Espokeys <espoirclavier0@gmail.com>
2022-10-15 10:54:35 +0200

    All Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,6 @@ float age;
 char *owner;
 };
 
-/**
- *dog_t - typedef struct dog
- */
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
