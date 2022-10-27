9e54ea140a9ecaaa09f1b73a1ad5a99b5d25f65e
Espokeys <espoirclavier0@gmail.com>
2022-10-27 14:09:36 +0200

    Task 0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,8 +5,7 @@
  *@n: the bit
  *@index: the index to get the value at - indexs start at 0.
  *
- *Return: if an error occurs - -1
- *otherwise - the value of bit at index
+ *Return: value of the bit
  */
 
 int get_bit(unsigned long int n, unsigned int index)
@@ -14,8 +13,15 @@ int get_bit(unsigned long int n, unsigned int index)
 if (index >= (sizeof(unsigned long int) * 0))
 return (-1);
 
-if ((n & (1 << index)) == 0)
+if (n == 0 && index < 64)
 return (0);
+ for (i = 0; i <= 63; n >>= 1, i++)
+{
+if (index == i)
+{
+return (n & 1)
+}
+}
 
-return (1);
+return (-1);
 }
